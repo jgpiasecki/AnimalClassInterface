88af6aa024a9533dd257d8fe8eaa6168d2f00157
John Piasecki <jgpie189@outlook.com>
2017-09-11 13:39:18 -0400

    added the bat class with its fields and methods

---
+++ src/animalclassinterface/Bat.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalclassinterface;
+
+/**
+ *
+ * @author jpiasecki
+ */
+public class Bat extends Mammal implements IFly, ISwim, IWalk, IMakeSound{
+    
+    String name = "Bat";
+    double bodyTemp = 105.8;
+    
+    public Bat(String name, double bodyTemp) {
+        super(name, bodyTemp);
+        
+        this.name=name;
+        this.bodyTemp=bodyTemp;
+    }
+    
+     @Override
+    public void fly()
+    {
+        System.out.println("The Bat flys.");
+    }
+    
+    @Override
+  public void swim()
+  {
+      System.out.println("The Bat swims.");
+  }
+  
+    @Override
+  public void walk()
+  {
+      System.out.println("The Bat walks.");
+  }
+    
+    @Override
+  public void makeSound()
+  {
+      System.out.println("The Bat squeaks.");
+  }
+}
