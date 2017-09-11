686a5c3b858f5f38757a03a20f49b0622529d8cd
John Piasecki <jgpie189@outlook.com>
2017-09-11 13:18:20 -0400

    Added the dolphin class and its fields and methods

---
+++ src/animalclassinterface/Dolphin.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animalclassinterface;
+
+/**
+ *
+ * @author jpiasecki
+ */
+public class Dolphin extends Mammal implements ISwim, IMakeSound{
+    String name = "Dolphin";
+    double bodyTemp = 98.6;
+    
+    public Dolphin(String name, int bodyTemp) {
+        super(name, bodyTemp);
+        this.name=name;
+        this.bodyTemp=bodyTemp;
+    }
+    
+    
+    
+      @Override
+  public void swim()
+  {
+      System.out.println("The Dolphin swims.");
+  }
+  
+  @Override
+  public void makeSound()
+  {
+      System.out.println("The Dolphin screeches.");
+  }
+}
