db53a0167cadf4c8feb8aa326fdb538da1c65b48
John Piasecki <jgpie189@outlook.com>
2017-09-12 14:10:08 -0400

    added the toString method

--- src/animalclassinterface/Mammal.java
+++ src/animalclassinterface/Mammal.java
@@ -24,5 +24,12 @@ public class Mammal extends Animal{
     public void setBodyTemp(double bodyTemp) {
         this.bodyTemp = bodyTemp;
     }
+
+    @Override
+    public String toString() {
+        return "Mammal{" + "bodyTemp=" + bodyTemp + '}';
+    }
+    
+    
     
 }
