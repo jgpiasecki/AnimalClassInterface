3d3d3613d03b8c319b4300e91919cf28b1c23cf1
John Piasecki <jgpie189@outlook.com>
2017-09-12 14:54:32 -0400

    added comments

--- src/animalclassinterface/Fish.java
+++ src/animalclassinterface/Fish.java
@@ -10,21 +10,26 @@ package animalclassinterface;
  * @author jpiasecki
  */
 public class Fish extends Animal {
+    //creates a boolean to see if the fish is salt water
     Boolean isSaltWater;
+    
+    //gets the name super and tells the saltwater to equal the boolean
     public Fish (String name, Boolean isSaltWater)
     {
         super(name);
         this.isSaltWater = isSaltWater;
     }
 
+    //gets the saltwater return
     public Boolean getIsSaltWater() {
         return isSaltWater;
     }
-
+    //sets the saltwater return
     public void setIsSaltWater(Boolean isSaltWater) {
         this.isSaltWater = isSaltWater;
     }
 
+    //makes sure that a string is returned
     @Override
     public String toString() {
         return "Fish{" + "isSaltWater=" + isSaltWater + '}';
