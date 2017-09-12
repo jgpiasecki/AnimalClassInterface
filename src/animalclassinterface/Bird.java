d11d8766cce85b54152305a34e377d54eb6155eb
John Piasecki <jgpie189@outlook.com>
2017-09-12 14:52:09 -0400

    added comments

--- src/animalclassinterface/Bird.java
+++ src/animalclassinterface/Bird.java
@@ -10,22 +10,27 @@ package animalclassinterface;
  * @author jpiasecki
  */
 public class Bird extends Animal{
+    //creates a double for the birds wingSpan
     double wingspan;
     
+    //gets the name super and tells the wingspan to equal the double
     public Bird(String name, double wingspan)
     {
         super(name);
         this.wingspan = wingspan;
     }
-
+    
+    //gets the wingspan
     public double getWingspan() {
         return wingspan;
     }
 
+    //sets the wingspan
     public void setWingspan(int wingspan) {
         this.wingspan = wingspan;
     }
 
+    //makes sure that a string is returned
     @Override
     public String toString() {
         return "Bird{" + "wingspan=" + wingspan + '}';
