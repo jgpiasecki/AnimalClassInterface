5e18ecddeffbbb5b4fecf01fd3ef47c49df8f793
John Piasecki <jgpie189@outlook.com>
2017-09-12 14:49:15 -0400

    added comments

--- src/animalclassinterface/Mammal.java
+++ src/animalclassinterface/Mammal.java
@@ -10,21 +10,27 @@ package animalclassinterface;
  * @author jpiasecki
  */
 public class Mammal extends Animal{
+    //creates a double called bodyTemp
     double bodyTemp;
+    
+    //gets the name super and sets the mammals bodyTemp equal to the double
     public Mammal(String name, double bodyTemp)
     {
         super(name);
         this.bodyTemp = bodyTemp;
     }
-
+    
+    //gets the bodyTemp
     public double getBodyTemp() {
         return bodyTemp;
     }
-
+    
+    //sets and returns the bodyTemp
     public void setBodyTemp(double bodyTemp) {
         this.bodyTemp = bodyTemp;
     }
-
+    
+    //makes sure the return is in string formate
     @Override
     public String toString() {
         return "Mammal{" + "bodyTemp=" + bodyTemp + '}';
